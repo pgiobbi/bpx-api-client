3c0232dfc2ec49155142f9e5a231f8aa0c4edfd4
pgiobbi <36854540+pgiobbi@users.noreply.github.com>
2025-08-12 10:33:59 +0200

    feat: add get_borrow_lend_markets

--- rust/client/src/routes/borrow_lend.rs
+++ rust/client/src/routes/borrow_lend.rs
@@ -1,14 +1,23 @@
-use bpx_api_types::borrow_lend::BorrowLendPosition;
+use bpx_api_types::borrow_lend::{BorrowLendMarket, BorrowLendPosition};
 
 use crate::{BpxClient, Result};
 
 #[doc(hidden)]
 pub const API_BORROW_LEND_POSITIONS: &str = "/api/v1/borrowLend/positions";
+#[doc(hidden)]
+pub const API_BORROW_LEND_MARKETS: &str = "/api/v1/borrowLend/markets";
 
 impl BpxClient {
     /// Retrieves all the open borrow lending positions for the account.
     pub async fn get_borrow_lend_positions(&self) -> Result<Vec<BorrowLendPosition>> {
         let url = format!("{}{}", self.base_url, API_BORROW_LEND_POSITIONS);
+        let res = self.get(url).await?;
+        res.json().await.map_err(Into::into)
+    }
+
+    /// Retrieves all borrow lending markets.
+    pub async fn get_borrow_lend_markets(&self) -> Result<Vec<BorrowLendMarket>> {
+        let url = format!("{}{}", self.base_url, API_BORROW_LEND_MARKETS);
         let res = self.get(url).await?;
         res.json().await.map_err(Into::into)
     }
--- rust/types/src/borrow_lend.rs
+++ rust/types/src/borrow_lend.rs
@@ -1,6 +1,6 @@
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
-
+use strum::{Display, EnumString};
 use crate::margin::MarginFunction;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -17,4 +17,35 @@ pub struct BorrowLendPosition {
     pub net_exposure_notional: Decimal,
     pub net_exposure_quantity: Decimal,
     pub net_quantity: Decimal,
+}
+
+#[derive(Debug, Display, Clone, Copy, Serialize, Deserialize, EnumString, PartialEq, Eq, Hash)]
+#[strum(serialize_all = "PascalCase")]
+#[serde(rename_all = "PascalCase")]
+pub enum BorrowLendMarketState {
+    Open,
+    Closed,
+    RepayOnly,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BorrowLendMarket {
+    pub state: BorrowLendMarketState,
+    pub asset_mark_price: Decimal,
+    pub borrow_interest_rate: Decimal,
+    pub borrowed_quantity: Decimal,
+    pub fee: Decimal,
+    pub lend_interest_rate: Decimal,
+    pub lent_quantity: Decimal,
+    pub max_utilization: Decimal,
+    pub open_borrow_lend_limit: Decimal,
+    pub optimal_utilization: Decimal,
+    pub symbol: String,
+    pub timestamp: chrono::DateTime<chrono::FixedOffset>,
+    pub throttle_utilization_threshold: Decimal,
+    pub throttle_utilization_bound: Decimal,
+    pub throttle_update_fraction: Decimal,
+    pub utilization: Decimal,
+    pub step_size: Decimal,
 }
